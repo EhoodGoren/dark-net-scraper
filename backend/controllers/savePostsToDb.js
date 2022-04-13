151469f4c080e768b58926805cb2ea72c5c67511
EhoodGoren <ehoodgoren@gmail.com>
2022-04-13 22:49:51 +0300

    Only new posts will be saved to db now
    Posts that have already been saved to the db will not be saved again

--- backend/controllers/savePostsToDb.js
+++ backend/controllers/savePostsToDb.js
@@ -1,9 +1,23 @@
-const mongoose = require('mongoose');
 const Post = require('../models/post');
+
+const findLatestBlog = async () => {
+    const latestBlog = await Post.find().sort({ date: -1 }).limit(1);
+    return latestBlog[0];
+};
+
+const filterExistingPosts = async (posts) => {
+    const latestBlog = await findLatestBlog();
+    if (!latestBlog) return posts;
+    return posts.filter((post) => {
+        return post.date > latestBlog.date;
+    });
+};
 
 const savePostsToDb = async (posts) => {
     try {
-        await Post.insertMany(posts);
+        const nonExistingPosts = await filterExistingPosts(posts);
+        if (nonExistingPosts.length === 0) return;
+        await Post.insertMany(nonExistingPosts);
     } catch (error) {
         throw error;
     }
