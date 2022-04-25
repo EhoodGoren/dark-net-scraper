d54fc5796221ed7c6a4cdc0b60424652bbdc9008
EhoodGoren <ehoodgoren@gmail.com>
2022-04-25 17:11:23 +0300

    Replaced loader with bootstrap loader

--- client/src/Loader/Loader.jsx
+++ client/src/Loader/Loader.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
-import loader from './loader.gif';
+import Spinner from 'react-bootstrap/Spinner';
 import './loader.css';
 
 const Loader = () => {
-    return <img className="loader" src={loader} alt={'loader'} />;
+    return (
+        <Spinner
+            className="loader"
+            animation="border"
+            style={{ width: '5rem', height: '5rem' }}
+        />
+    );
 };
 
 export default Loader;
